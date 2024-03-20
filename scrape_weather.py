418091da5a7ee29b1f4af98b991bee3be963b099
j-hamilton3 <118838540+j-hamilton3@users.noreply.github.com>
2024-03-20 13:22:19 -0500

    Added module docstring.

--- scrape_weather.py
+++ scrape_weather.py
@@ -1,8 +1,8 @@
 """
-Description:
-Author:
-Section Number:
-Date Created:
-Credit: 
+Description: A Weather Scraper created for Milestone 1.
+Author: James Hamilton
+Section Number: ADEV-3005, N. Cai, FTO01
+Date Created: March 20th, 2024
+Credit:
 Updates:
 """
