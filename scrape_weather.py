1dcf8e1b8423428df5f0ea15717c1a02934d9f67
j-hamilton3 <118838540+j-hamilton3@users.noreply.github.com>
2024-03-20 13:58:00 -0500

    Added starting_url with todays date.

--- scrape_weather.py
+++ scrape_weather.py
@@ -6,3 +6,18 @@ Date Created: March 20th, 2024
 Credit:
 Updates:
 """
+from datetime import datetime
+
+# Get the current date and time.
+now = datetime.now()
+
+current_year = now.year
+current_month = now.month
+current_day = now.day
+print(f"Year: {current_year}, Month: {current_month}, Day: {current_day}")
+
+starting_url = f"http://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174" \
+               f"&timeframe=2&StartYear=1840&EndYear={current_year}&Day={current_day}&" \
+               f"Year={current_year}&Month={current_month}"
+
+print(starting_url)
