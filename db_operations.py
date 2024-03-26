66f25012cc255eaea11748bb653b7262312b1ead
j-hamilton3 <118838540+j-hamilton3@users.noreply.github.com>
2024-03-25 19:36:41 -0500

    Added module level comment.

--- db_operations.py
+++ db_operations.py
@@ -1,8 +1,8 @@
 """
-Description:
-Author:
-Section Number:
-Date Created:
-Credit: 
+Description: Part 2 - Database for Final Project.
+Author: James Hamilton
+Section Number: ADEV-3005, N. Cai, FTO01
+Date Created: March 25th, 2024
+Credit:
 Updates:
 """
