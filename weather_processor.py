662bfa78b9ca6d212aa808e50a68273716dc3814
j-hamilton3 <118838540+j-hamilton3@users.noreply.github.com>
2024-03-27 20:50:56 -0500

    Adjusted update_dates to track yesterday.

--- weather_processor.py
+++ weather_processor.py
@@ -6,37 +6,38 @@ Date Created: March 27, 2024
 Credit:
 Updates:
 """
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from db_operations import DBOperations
 
 def update_dates():
     """Returns months of available weather data."""
-    # Grab latest date of data from DB.
+    # Grab the latest date of data from DB.
     db = DBOperations()
     latest_date_str = db.check_latest_data()
 
     latest_date = datetime.strptime(latest_date_str, "%Y-%m-%d")
 
-    today = datetime.now()
+    # Get yesterdays date. As user will never have todays weather data.
+    yesterday = datetime.now() - timedelta(days=1)
 
-    # Check if today and latest_date are in the same month and year.
-    if today.year == latest_date.year and today.month == latest_date.month:
-        if today == latest_date:
-            return []  # If dates are the same, return empty list.
+    # Check if yesterday and latest_date are in the same month and year
+    if yesterday.year == latest_date.year and yesterday.month == latest_date.month:
+        if yesterday == latest_date:
+            return []  # If dates are the same, return empty list
         else:
-            return [today.strftime("%Y-%m")]
+            return [yesterday.strftime("%Y-%m")]
         # If in the same month but not the same day, include the current month.
 
-    # If today is greater than the latest_date and they are not in the same month.
+    # If yesterday is greater than the latest_date and they are not in the same month.
     months_list = []
 
     # Adding latest date to current date for iteration.
     current_date = latest_date
 
-    while current_date <= today:
+    while current_date <= yesterday:
         months_list.append(current_date.strftime("%Y-%m"))
-        current_date += relativedelta(months=+1)  # Increment month by one.
+        current_date += relativedelta(months=+1)  # Increment month by one
 
     return months_list
 
